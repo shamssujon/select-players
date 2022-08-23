178d578048a3a02e0b46590056795f638c2ccb18
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-08-23 23:07:54 +0600

    Total cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+// Get number from Input
+function getNumberFromInput(inputId){
+    const element = document.getElementById(inputId); 
+    const elementValue = element.value;
+    const elementValueNumber = parseFloat(elementValue);
+    return elementValueNumber;
+}
+
+// Get number from HTML element
+function getNumberFromElement(elementId){
+    const element = document.getElementById(elementId); 
+    const elementValue = element.innerText;
+    const elementValueNumber = parseFloat(elementValue);
+    return elementValueNumber;
+}
+
 // Budget calculation
 
 // Player expenses
@@ -5,13 +21,20 @@ const playerCostButton = document.getElementById("player-cost-button");
 
 playerCostButton.addEventListener("click", function () {
     const totalPlayer = 5;
-
-    const perPlayerCostElement = document.getElementById("perPlayerCost");
-    const perPlayerCostString = perPlayerCostElement.value;
-    const perPlayerCost = parseFloat(perPlayerCostString);
-    
+    const perPlayerCost = getNumberFromInput("perPlayerCost");
     const playerCostTotal = perPlayerCost * totalPlayer;
-    
     const playerExpensesElement = document.getElementById("playerExpenses");
     playerExpensesElement.value = playerCostTotal;
 });
+
+const totalCostButton = document.getElementById("total-cost-button");
+
+totalCostButton.addEventListener("click", function(){
+    const playerCostTotal = getNumberFromInput("playerExpenses");
+    const managerCost = getNumberFromInput("managerCost");
+    const coachCost = getNumberFromInput("coachCost");
+    const totalCost = playerCostTotal + managerCost + coachCost;
+
+    const totalCostElement = document.getElementById("totalCost");
+    totalCostElement.value = totalCost;
+})
